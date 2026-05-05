5502d9ff2a8100ab0c7c1d89085d1467efe162f3
Léo Augusto <leoaugustopi@gmail.com>
2026-05-05 19:38:02 -0300

    feat(web): add BasePage and Login page object

---
+++ web_tests/pages/base.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+class BasePage:
+    def __init__(self, driver, timeout=10):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
+
+    def find(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def click(self, locator):
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
+
+    def type(self, locator, text):
+        element = self.find(locator)
+        element.clear()
+        element.send_keys(text)
+
+    def text_of(self, locator):
+        return self.find(locator).text
---
+++ web_tests/pages/login.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+
+from pages.base import BasePage
+
+
+class LoginPage(BasePage):
+    URL = "https://www.saucedemo.com/"
+    USERNAME = (By.ID, "user-name")
+    PASSWORD = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR = (By.CSS_SELECTOR, "[data-test='error']")
+
+    def open(self):
+        self.driver.get(self.URL)
+
+    def login(self, username, password):
+        self.type(self.USERNAME, username)
+        self.type(self.PASSWORD, password)
+        self.click(self.LOGIN_BUTTON)
+
+    def error_message(self):
+        return self.text_of(self.ERROR)
